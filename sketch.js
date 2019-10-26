36b3cc171e4293cc242a5edca3ca147938bfe7d5
Kimmy <randomproductions58@gmail.com>
2019-10-26 13:36:20 -0400

    front end
    front end

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let img;
+
+function preload(){
+      createCanvas(720, 400);
+  img = loadImage(document.getElementById("myImage").innerHTML); // Load the image
+
+}
+function readURL(input){
+    if(input.files && input.files[0]){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#myImage').attr('src', e.target.result)
+            .width(150).height(200)
+        };
+        reader.readAsDataURL(input.files[0]);
+            
+        }
+    }
+
+function selectImage(){
+    
+}
+function setup(){
+    
+}
+function draw(){
+   // image(document.getElementById("imageforpancake"), 0,0);
+    //  img = loadImage(document.getElementById("myImage").innerHTML); // Load the image
+
+     image(img, 0,0);
+}
